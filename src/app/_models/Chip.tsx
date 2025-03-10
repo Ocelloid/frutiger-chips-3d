006fa5c2d338c639181aa9ac7a849bcdf7890ed2
Nikita Grebnev <ocelloid@gmail.com>
2025-03-10 22:04:00 +0500

    chips are moving

---
+++ src/app/_models/Chip.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useState, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { MeshTransmissionMaterial, useGLTF } from "@react-three/drei";
+import type { Mesh, Euler, EulerOrder } from "three";
+import { Vector3 } from "three";
+import { easing } from "maath";
+
+export default function Chip({
+  rotation,
+  position,
+  scale,
+}: {
+  rotation?:
+    | Euler
+    | [x: number, y: number, z: number, order?: EulerOrder | undefined];
+  position?: Vector3 | [x: number, y: number, z: number];
+  scale?: Vector3 | [x: number, y: number, z: number];
+}) {
+  const chipRef = useRef<Mesh>();
+  const [hovered, setHovered] = useState(false);
+  const { nodes } = useGLTF("/models/ЧИПС1.glb");
+  const chipMesh = nodes.Box014! as Mesh;
+
+  useFrame((state, delta) => {
+    if (chipRef.current && hovered) {
+      easing.dampE(
+        chipRef.current.rotation,
+        [Math.PI * state.pointer.y, -Math.PI * state.pointer.x, 0],
+        0.1,
+        delta * 0.1,
+      );
+    }
+  });
+
+  return (
+    <mesh
+      scale={scale ?? new Vector3(0.01, 0.009, 0.0075)}
+      onPointerEnter={() => setHovered(true)}
+      onPointerLeave={() => setHovered(false)}
+      geometry={chipMesh.geometry}
+      position={position}
+      rotation={rotation}
+      ref={chipRef}
+    >
+      <MeshTransmissionMaterial
+        thickness={1}
+        roughness={0}
+        transmission={1}
+        ior={1}
+        chromaticAberration={0.2}
+        backside={false}
+      />
+    </mesh>
+  );
+}
+
+useGLTF.preload("/models/ЧИПС1.glb");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import Chip from "./models/Chip";
+import Chip from "./_models/Chip";
 import { PointLight } from "three";
-import Space from "./models/Space";
+import Space from "./_models/Space";
 
 export default function HomePage() {
-  const sun = new PointLight("white", 100);
+  const sun = new PointLight("white", 1000);
   sun.position.set(-4, 3, -4);
 
-  const light = new PointLight("lightpink", 30);
+  const light = new PointLight("lightpink", 300);
   light.position.set(-5, 5, 5);
 
-  const backlight1 = new PointLight("orangered", 10);
+  const backlight1 = new PointLight("orangered", 100);
   backlight1.position.set(10, 5, -15);
-  const backlight2 = new PointLight("orangered", 10);
+  const backlight2 = new PointLight("orangered", 100);
   backlight2.position.set(15, -10, -20);
 
   return (
@@ -35,9 +34,27 @@ export default function HomePage() {
           scene.add(camera);
         }}
       >
-        <OrbitControls />
         <Space />
-        <Chip />
+        <Chip
+          position={[4, -1, -3 * Math.random()]}
+          rotation={[0, 0, Math.PI * Math.random()]}
+        />
+        <Chip
+          position={[2, 1, -1 * Math.random()]}
+          rotation={[0, 0, Math.PI * Math.random()]}
+        />
+        <Chip
+          position={[0, -2, -3 * Math.random()]}
+          rotation={[0, 0, Math.PI * Math.random()]}
+        />
+        <Chip
+          position={[-2, 1, -1 * Math.random()]}
+          rotation={[0, 0, Math.PI * Math.random()]}
+        />
+        <Chip
+          position={[-4, -1, -3 * Math.random()]}
+          rotation={[0, 0, Math.PI * Math.random()]}
+        />
       </Canvas>
     </main>
   );
